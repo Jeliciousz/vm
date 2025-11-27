c57e6e716687b608c891fcb0f445b1bb7961ed3e
Jeliciousz <jeliciousz@proton.me>
2025-11-27 10:43:23 -0500

    reorder fields

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,23 +3,23 @@ use crate::memory::MemoryController;
 const RESET_VECTOR: usize = 0xFFFE;
 
 pub struct CPU<'cpu> {
+    pub memory_controller: MemoryController<'cpu>,
     pub instruction_pointer: u16,
     pub accumulator: u8,
-    pub memory_controller: MemoryController<'cpu>,
 }
 
 impl<'cpu> CPU<'cpu> {
     pub fn new() -> Self {
         Self {
+            memory_controller: MemoryController::new(),
             instruction_pointer: 0x0000,
             accumulator: 0x00,
-            memory_controller: MemoryController::new(),
         }
     }
 
     pub fn reset(&mut self) {
-        self.accumulator = 0x00;
         self.memory_controller.reset();
         self.instruction_pointer = self.memory_controller.read_16(RESET_VECTOR);
+        self.accumulator = 0x00;
     }
 }
